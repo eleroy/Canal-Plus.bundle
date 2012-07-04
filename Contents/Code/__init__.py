e094545657a531c5a46e00bb53dc19d885a4201a
Mike <mike.d.meier@gmail.com>
2012-07-04 12:08:23 -0400

    initial v2.1 upgrade

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,18 +1,14 @@
-# -*- coding: utf-8 -*-
-
 BASE_URL = "http://webservice.canal-plus.com/rest/bigplayer/"
 
 def Start():
 	Plugin.AddPrefixHandler("/video/canalplus", ListeCategories, "Canal Plus", "icon-default.png", "art-default.jpg")
-	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-	MediaContainer.title1    = 'Canal Plus'
-	MediaContainer.viewGroup = 'InfoList'
-	MediaContainer.art       = R("art-default.jpg")
+	ObjectContainer.title1    = 'Canal Plus'
+	ObjectContainer.art       = R("art-default.jpg")
 
 
 #Root categories
 def ListeCategories():
-	dir = MediaContainer()
+	oc = ObjectContainer()
 	categories = XML.ElementFromURL(BASE_URL + 'initPlayer').xpath('//THEMATIQUE')
 	for categorie in categories:
 		nom = categorie.xpath("./NOM")[0].text.capitalize()
@@ -21,17 +17,16 @@ def ListeCategories():
 		if icon == None:
 			icon = R('icon-folder.png')
 
-		dir.Append(Function(DirectoryItem(ListeSousCategories, title = nom, thumb = icon), idCategorie = idCategorie, nomCategorie = nom))
+        oc.add(DirectoryObject(key=Callback(ListeSousCategories, idCategorie=idCategorie, nomCategorie=nom), title=nom, thumb=icon))
 
-	dir.Append(PrefsItem("Préférences", thumb = R('icon-folder.png')))
-	return dir
+	return oc
 
 #Sub-categories
-def ListeSousCategories(sender, idCategorie, nomCategorie):
+def ListeSousCategories(idCategorie, nomCategorie):
 	art = R("art-cat"+idCategorie+".png")
 	if art == None:
 		art = R("art-default.jpg")
-	dir = MediaContainer(title1 = "Canal Plus", title2 = nomCategorie, art = art)
+	oc = ObjectContainer(title2 = nomCategorie, art = art)
 	sousCategories = XML.ElementFromURL(BASE_URL + 'initPlayer').xpath("//THEMATIQUE[ID="+idCategorie+"]//SELECTIONS")[0]
 	for sousCategorie in sousCategories:
 		nom = sousCategorie.xpath("./NOM")[0].text.capitalize()
@@ -40,13 +35,13 @@ def ListeSousCategories(sender, idCategorie, nomCategorie):
 		if icon == None:
 			icon = R('icon-folder.png')
 
-		dir.Append(Function(DirectoryItem(ListeVideos, title = nom, thumb = icon), idSousCategorie = idSousCategorie, nomSousCategorie = nom, art = art))
+        oc.add(DirectoryObject(key=Callback(ListeVidoes, idSousCategorie=idSousCategorie, nomSousCategorie=nom, art=art), title=nom, thumb=icon))
 
-	return dir
+	return oc
 
 #Chosen sub-category's videos
-def ListeVideos(sender, idSousCategorie, nomSousCategorie, art):
-	dir = MediaContainer(title1 = "Canal Plus", title2 = nomSousCategorie, art = art)
+def ListeVideos(idSousCategorie, nomSousCategorie, art):
+	oc = ObjectContainer(title2 = nomSousCategorie, art = art)
 	videos = XML.ElementFromURL(BASE_URL + "getMEAs/" + idSousCategorie).xpath("//MEA[TYPE!='CHAINE LIVE']")
 	for video in videos:
 		idVideo = video.xpath('./ID')[0].text
@@ -57,13 +52,14 @@ def ListeVideos(sender, idSousCategorie, nomSousCategorie, art):
 
 		description = video.xpath('./INFOS/DESCRIPTION')[0].text
 		thumb = video.xpath('./MEDIA/IMAGES/GRAND')[0].text
-		dir.Append(Function(DirectoryItem(ListeVideosLiees, title = titre, summary = description, thumb = thumb), idVideo = idVideo, nomSousCategorie = nomSousCategorie, art = art))
+        oc.add(DirectoryObject(key=Callback(ListeVideosLiees, idVideo=idVideo, nomSousCategorie=nomSousCategorie, art=art), title=titre, summary=description,
+            thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback="icon-default.png"))
 
-	return dir
+	return oc
 
 #Video chosen + related videos
-def ListeVideosLiees(sender, idVideo, nomSousCategorie, art):
-	dir = MediaContainer(title1 = "Canal Plus", title2 = nomSousCategorie, art = art)
+def ListeVideosLiees(idVideo, nomSousCategorie, art):
+	oc = ObjectContainer(title2 = nomSousCategorie, art = art)
 
 	videosXml = XML.ElementFromURL(BASE_URL + "getVideosLiees/" + idVideo)
 	videos = videosXml.xpath("//VIDEO[ID='"+idVideo+"']")
@@ -77,77 +73,8 @@ def ListeVideosLiees(sender, idVideo, nomSousCategorie, art):
 
 		description = video.xpath('./INFOS/DESCRIPTION')[0].text
 		thumb = video.xpath('.//MEDIA/IMAGES/GRAND')[0].text
-		dir.Append(ElementVideo(video, titre, description, thumb))
+        video_url = '' ##find the video url and pass it to the URL Service
+        oc.add(VideoClipObject(url=video_url, title=titre, summary=description,
+            thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback="icon-default.png")))
 
-	return dir
-
-#Create the correct video element depending on the video's URL
-def ElementVideo(video, titre, description, thumb):
-
-	#Available qualities
-	try:
-		lienHD = video.xpath('.//MEDIA/VIDEOS/HD')[0].text
-	except:
-		lienHD = None
-
-	try:
-		lienHQ = video.xpath('.//MEDIA/VIDEOS/HAUT_DEBIT')[0].text
-	except:
-		lienHQ = None
-
-	try:
-		lienLQ = video.xpath('.//MEDIA/VIDEOS/BAS_DEBIT')[0].text
-	except:
-		lienLQ = None
-
-	#Use the wanted quality, if it exists. If not use a lower quality
-	qualitePreferee = Prefs['qualite']
-	if qualitePreferee == 'LQ':
-		lien = lienLQ
-
-	elif qualitePreferee == 'HQ':
-		if lienHQ == None:
-			lien = lienLQ
-		else:
-			lien = lienHQ
-
-	elif qualitePreferee == 'HD':
-		if lienHD == None:
-			if lienHQ == None:
-				lien = lienLQ
-			else:
-				lien = lienHQ
-
-		else:
-			lien = lienHD
-
-	#If RTMP, get url and clip parts
-	if lien.startswith("rtmp://"):
-		if lien.startswith("rtmp://vod-fms.canalplus.fr/ondemand"):
-			lien = lien.replace("rtmp://vod-fms.canalplus.fr/ondemand/","")
-			url = "rtmp://vod-fms.canalplus.fr/ondemand"
-
-		if lien.startswith("rtmp://geo2-vod-fms.canalplus.fr/ondemand"):
-			lien = lien.replace("rtmp://geo2-vod-fms.canalplus.fr/ondemand","")
-			url = "rtmp://geo2-vod-fms.canalplus.fr/ondemand"
-			
-		if lien.startswith("rtmp://geo4-vod-fms.canalplus.fr/ondemand"):
-			lien = lien.replace("rtmp://geo4-vod-fms.canalplus.fr/ondemand","")
-			url = "rtmp://geo4-vod-fms.canalplus.fr/ondemand"
-			
-		if lien.startswith("rtmp://geo5-vod-fms.canalplus.fr/ondemand"):
-			lien = lien.replace("rtmp://geo5-vod-fms.canalplus.fr/ondemand","")
-			url = "rtmp://geo5-vod-fms.canalplus.fr/ondemand"
-
-		if lien.endswith(".mp4"):
-			lien = "mp4:/" + lien	
-			lien = lien[:-4]
-
-		elif lien.endswith(".flv"):
-			lien = lien[:-4]
-
-	#Regular VideoItem if video available through http. RTMPVideoItem if not.
-	if lien.startswith("http://"):
-		return VideoItem(lien, titre, summary = description, thumb = thumb)
-	else:
-		return RTMPVideoItem(url = url, width=640, height=375, clip = lien, title = titre, summary = description, thumb = thumb)	
+	return oc
